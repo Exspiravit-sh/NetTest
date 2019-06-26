6ff9cdfaa44ee6ff7ec138dd63fe8cf77a99315b
exspiravit-sh <exspiravit-sh@protonmail.com>
2019-06-26 09:06:39 +0100

    modified:   README.md 	modified:   nettest.sh 	deleted:    nettest.txt 	deleted:    nmap.txt 	deleted:    speed.txt

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
-# NetTest - Network Test
+## NetTest - Network Test
+### Developed By Exspiravit.sh (https://github.com/exspiravit-sh)
+
 #### A Simple Network Testing Script. Just install Nmap And Run "nettest.sh" To Start.
+No known issues at this current time, though everything has been 
+improved to run faster and to hopefully be easier to read.
+
+### Usage:
 ```
 git clone https://github.com/Exspiravit-sh/NetTest
 cd NetTest
 sudo apt-get install nmap
+sudo bash nettest.sh
 ```
 
-#### Donations:
-#PayPal:
+## Donations:
+#### PayPal (Exspiravit-sh):
 https://paypal.me/pools/c/8bB25QBenI
-https://www.paypal.me/exspiravit
--- nettest.sh
+++ nettest.sh
@@ -6,9 +6,12 @@
 #https://www.github.com/exspiravit-sh
 
 clear
-rm nettest.txt
+rm .nettest.txt
+rm .speed.txt
+rm .nmap.txt
 sleep 0.5
 printf "Please Wait."
+
 check() {
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
 clear
@@ -16,71 +19,50 @@ printf "\nPlease Run This Script As Root.\n"
 exit
 fi
 }
+
 logo() {
 cat logo.ascii
 }
+
 system() {
 printf "Linux Distro: "
 cat /etc/issue
 }
+
 get() {
-printf "."
-sleep 0.5
-printf "."
-sleep 0.5
-printf "."
-sleep 0.5
-printf "."
-curl -s wtfismyip.com/json > nettest.txt
-ping 127.0.0.1 -c 10 > speed.txt
+curl -s wtfismyip.com/json > .nettest.txt
+ping 127.0.0.1 -c 10 > .speed.txt
 clear
 printf "Finished.\n"
 sleep 0.5
 clear
-nmap 127.0.0.1 -oN nmap.txt
+nmap 127.0.0.1 -oN .nmap.txt
 clear
 }
+
 ip() {
+printf "##############################################\n"
 printf "IP:"
-grep -a 'IP' nettest.txt | cut -d ":" -f2 | tr -d ',"'
-}
-location() {
+grep -a 'IP' .nettest.txt | cut -d ":" -f2 | tr -d ',"'
 printf "Location:"
-grep -a 'Location' nettest.txt | cut -d ":" -f2 | tr -d ',"'
-}
-hostname() {
+grep -a 'Location' .nettest.txt | cut -d ":" -f2 | tr -d ',"'
 printf "Hostname:"
-grep -a 'Hostname' nettest.txt | cut -d ":" -f2 | tr -d ',"'
-}
-isp() {
+grep -a 'Hostname' .nettest.txt | cut -d ":" -f2 | tr -d ',"'
 printf "ISP:"
-grep -a 'ISP' nettest.txt | cut -d ":" -f2 | tr -d ',"'
-}
-proxy() {
+grep -a 'ISP' .nettest.txt | cut -d ":" -f2 | tr -d ',"'
 printf "Proxy/Exit-Node:"
-grep -a 'TorExit' nettest.txt | cut -d ":" -f2 | tr -d ',"'
-}
-countrycode() {
+grep -a 'TorExit' .nettest.txt | cut -d ":" -f2 | tr -d ',"'
 printf "Country Code:"
-grep -a 'Country' nettest.txt | cut -d ":" -f2 | tr -d ',"'
-}
-speed() {
+grep -a 'Country' .nettest.txt | cut -d ":" -f2 | tr -d ',"'
 printf "Bandwidth Speed:"
-grep -a 'rtt' speed.txt | tr -d "rtt"
-}
-port() {
+grep -a 'rtt' .speed.txt | tr -d "rtt"
+printf "##############################################\n"
 printf "Running Services:\n"
-sed '/# /d' nmap.txt | cut -d 'N' -f1 | cut -d 'H' -f1 | grep -a ' '
+sed '/# /d' .nmap.txt | cut -d 'N' -f1 | cut -d 'H' -f1 | grep -a ' '
 }
+
 check
 get
 logo
 system
 ip
-location
-hostname
-isp
-proxy
-countrycode
-speed
-port
